d2c1c405e5da2ae980820c5b0411075b7979ddc7
LukiaEdmiidz <tuckahoelukia@gmail.com>
2023-11-01 16:18:39 -0700

    added swipe left

--- RapidEmoji-Race/RapidEmoji-Race/ContentView.swift
+++ RapidEmoji-Race/RapidEmoji-Race/ContentView.swift
@@ -14,14 +14,40 @@ import SwiftUI
 struct ContentView: View {
     @State private var showWord: Bool = false
     @State private var currentIndex: Int = 0
-    
+    @State private var offset: CGSize = .zero
+
+
     var body: some View {
         VStack(spacing: 20) {
             // The emoji card
             FlashcardView(flashcard: flashcards[currentIndex], showWord: showWord)
-                .onTapGesture {
-                    showWord.toggle()
-                }
+                .offset(offset)
+                .gesture(
+                    DragGesture()
+                        .onChanged { gesture in
+                            self.offset = gesture.translation
+                        }
+                        .onEnded { _ in
+                            if self.offset.width > 100 {  // swiped right
+                                self.showWord = false
+                                self.nextCard()
+                            } else if self.offset.width < -100 {  // swiped left
+                                self.showWord = false
+                                self.nextCard()
+                            }
+                            self.offset = .zero
+                        }
+                )
+                .simultaneousGesture(TapGesture()
+                                        .onEnded { _ in
+                                            self.showWord.toggle()
+                                        })
+
+
+
+
+
+
 
             // Conditionally render the word or the hint
             if showWord {
@@ -41,6 +67,17 @@ struct ContentView: View {
             showWord = false
         }
     }
+    
+    func nextCard() {
+        if currentIndex < flashcards.count - 1 {
+            currentIndex += 1
+        } else {
+            currentIndex = 0
+        }
+    }
+
+
+
 }
 
 
