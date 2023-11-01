7ac1f4d282e44b68270524f7d59c7f431bc36337
LukiaEdmiidz <tuckahoelukia@gmail.com>
2023-11-01 13:31:49 -0700

    Initial commit

---
+++ RapidEmoji-Race/RapidEmoji-Race/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  RapidEmoji-Race
+//
+//  Created by Lukia Edmiidz on 11/1/23.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
