ab4f84906b1e833fb67fb78919e445e1dc2ceb7a
Nik Edmiidz <edmiidz@gmail.com>
2024-03-23 21:17:54 -0700

    adding FlashcardView.swift
    adding FlashcardView.swift

---
+++ EmojiRealmDB/EmojiRealmDB/FlashcardView.swift
@@ -0,0 +1,40 @@
+//
+//  FlashcardView.swift
+//  RapidEmoji-Race
+//
+//  Created by Lukia Edmiidz on 11/1/23.
+//
+
+import SwiftUI
+
+struct FlashcardView: View {
+    var flashcard: Flashcard
+    var showWord: Bool  // Added this to control word display
+
+    var body: some View {
+        VStack(spacing: 20) {
+            Text(flashcard.emoji)
+                .font(.system(size: 200)) // Adjust the size as desired.
+                .padding()
+
+            if showWord {  // Only show the word if showWord is true
+                Text(flashcard.english)
+                    .font(.title)
+                    .padding()
+            }
+        }
+        .padding()
+    }
+}
+
+struct Flashcard {
+    var emoji: String
+    var english: String
+}
+
+
+
+
+
+
+
